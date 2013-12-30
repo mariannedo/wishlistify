04bc9eb637ec42ea9022857dda303365d9bce0f2
mariannegreat <mariannepdo@gmail.com>
2013-12-29 20:56:01 -0500

    some initial styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
   	@products = ShopifyAPI::Product.all
 
-  	import_new_customers
-  	import_new_products
+  	#import_new_customers
+  	#import_new_products
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Wishlistify::Application.routes.draw do
   get "user/index"
   root "home#index"
   #get "/auth/:provider", to: "sessions#create"
-  resources :users, :products, :user_products
+  resources :users, path: '/wishlist'
+  resources :products, :user_products
 end
