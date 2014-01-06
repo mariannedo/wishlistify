27e7d9d8aed38d6fc9ff1fca87d76410f8ee7290
mariannegreat <mariannepdo@gmail.com>
2014-01-06 18:24:58 -0500

    tweaks?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
-  def index
-  	@products = ShopifyAPI::Product.all
-  	#import_new_customers
-  	#import_new_products
-  end
+	def index
+		@products = ShopifyAPI::Product.all
+		#import_new_customers
+		#import_new_products
+	end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :shopify, ENV['SHOPIFY_API_KEY'], ENV['SHOPIFY_SHARED_SECRET'],
-#   			:scope => 'read_products,read_orders,write_content',
-#             :setup => lambda { |env| params = Rack::Utils.parse_query(env['QUERY_STRING'])
-#                                      env['omniauth.strategy'].options[:client_options][:site] = "https://outdoor-apparel.myshopify.com" }
-# end
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :shopify, ENV['SHOPIFY_API_KEY'], ENV['SHOPIFY_SHARED_SECRET'],
+  			:scope => 'read_products,write_content,read_customers,write_customers',
+            :setup => lambda { |env| params = Rack::Utils.parse_query(env['QUERY_STRING'])
+                                     env['omniauth.strategy'].options[:client_options][:site] = "https://outdoor-apparel.myshopify.com" }
+end
