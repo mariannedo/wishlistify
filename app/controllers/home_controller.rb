faa22716accfde64dd1fc6d5d6851442927c3f1d
mariannegreat <mariannepdo@gmail.com>
2014-01-09 19:20:33 -0500

    attempting to fix home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
 	def index
-		#@products = ShopifyAPI::Product.all
-		#import_new_customers
-		#import_new_products
+		@products = ShopifyAPI::Product.all
+		import_new_customers
+		import_new_products
 		redirect_to 'http://outdoor-apparel.myshopify.com/account/login?wishlist'
 	end
 end
